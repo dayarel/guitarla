b23d44cf38c3364ad6f005dbea2668750a7f942f
David Cortes <dayareldev@gmail.com>
2025-01-07 01:40:08 +0100

    Funcionalidades del carrito

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,14 @@ function App() {
   const [data, setData] = useState(db);
   const [cart, setCart] = useState([]);
 
+  const MAX_ITEMS = 5;
+  const MIN_ITEMS = 1;
+
   function addToCart(item) {
     const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
 
     if (itemExists >= 0) {
+      if (cart[itemExists].quantity >= MAX_ITEMS) return;
       const updatedCart = [...cart];
       cart[itemExists].quantity++;
       setCart(updatedCart);
@@ -23,9 +27,50 @@ function App() {
     }
   }
 
+  function removeFromCart(id) {
+    setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
+  }
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1,
+        };
+      }
+      return item;
+    });
+
+    setCart(updatedCart);
+  }
+
+  function decreaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity > MIN_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity - 1,
+        };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
+
+  function deleteCart() {
+    setCart([]);
+  }
+
   return (
     <>
-      <Header cart={cart} />
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        deleteCart={deleteCart}
+      />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,18 @@
-export default function Header({ cart }) {
+import { useMemo } from "react";
+
+export default function Header({
+  cart,
+  removeFromCart,
+  increaseQuantity,
+  decreaseQuantity,
+  deleteCart,
+}) {
   // State Derivado
-  const isEmpty = () => cart.length === 0;
+  const isEmpty = useMemo(() => cart.length === 0, [cart]);
+  const cardTotal = useMemo(
+    () => cart.reduce((total, item) => total + item.quantity * item.price, 0),
+    [cart]
+  );
 
   return (
     <header className="py-5 header">
@@ -24,55 +36,71 @@ export default function Header({ cart }) {
               />
 
               <div id="carrito" className="bg-white p-3">
-                {isEmpty() ? (
+                {isEmpty ? (
                   <p className="text-center">El carrito esta vacio</p>
                 ) : (
-                  <table className="w-100 table">
-                    <thead>
-                      <tr>
-                        <th>Imagen</th>
-                        <th>Nombre</th>
-                        <th>Precio</th>
-                        <th>Cantidad</th>
-                        <th></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {cart.map((guitar) => (
-                        <tr key={guitar.id}>
-                          <td>
-                            <img
-                              className="img-fluid"
-                              src={`img/${guitar.image}.jpg`}
-                              alt="imagen guitarra"
-                            />
-                          </td>
-                          <td>{guitar.name}</td>
-                          <td className="fw-bold">${guitar.price}</td>
-                          <td className="flex align-items-start gap-4">
-                            <button type="button" className="btn btn-dark">
-                              -
-                            </button>
-                            {guitar.quantity}
-                            <button type="button" className="btn btn-dark">
-                              +
-                            </button>
-                          </td>
-                          <td>
-                            <button className="btn btn-danger" type="button">
-                              X
-                            </button>
-                          </td>
+                  <>
+                    <table className="w-100 table">
+                      <thead>
+                        <tr>
+                          <th>Imagen</th>
+                          <th>Nombre</th>
+                          <th>Precio</th>
+                          <th>Cantidad</th>
+                          <th></th>
                         </tr>
-                      ))}
-                    </tbody>
-                  </table>
+                      </thead>
+                      <tbody>
+                        {cart.map((guitar) => (
+                          <tr key={guitar.id}>
+                            <td>
+                              <img
+                                className="img-fluid"
+                                src={`img/${guitar.image}.jpg`}
+                                alt="imagen guitarra"
+                              />
+                            </td>
+                            <td>{guitar.name}</td>
+                            <td className="fw-bold">${guitar.price}</td>
+                            <td className="flex align-items-start gap-4">
+                              <button
+                                onClick={() => decreaseQuantity(guitar.id)}
+                                type="button"
+                                className="btn btn-dark"
+                              >
+                                -
+                              </button>
+                              {guitar.quantity}
+                              <button
+                                onClick={() => increaseQuantity(guitar.id)}
+                                type="button"
+                                className="btn btn-dark"
+                              >
+                                +
+                              </button>
+                            </td>
+                            <td>
+                              <button
+                                onClick={() => removeFromCart(guitar.id)}
+                                className="btn btn-danger"
+                                type="button"
+                              >
+                                X
+                              </button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                    <p className="text-end">
+                      Total pagar: <span className="fw-bold">${cardTotal}</span>
+                    </p>
+                  </>
                 )}
-
-                <p className="text-end">
-                  Total pagar: <span className="fw-bold">$899</span>
-                </p>
-                <button className="btn btn-dark w-100 mt-3 p-2">
+                <button
+                  onClick={deleteCart}
+                  className="btn btn-dark w-100 mt-3 p-2"
+                >
                   Vaciar Carrito
                 </button>
               </div>
